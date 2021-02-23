69eaf6ff4fa44b74bacb79c808065c64e0b3106a
BaptisteGarcin <baptistabomb86@gmail.com>
2021-02-23 18:56:38 +0100

    change proxy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect } from 'react'
+import React, {useState, useEffect, useRef } from 'react'
 import './App.css';
 import yahooFinance from './yahoo-finance';
 
 const App = () => {
-
+  const isInitialMount = useRef(true);
   const [ticker, setTicker] = useState("");
   const [earningsGain, setEarningsGain] = useState();
 
@@ -15,14 +15,20 @@ const App = () => {
   const getProfits = () => {
     yahooFinance.quote({
       symbol: `${ticker}.PA`,
-      modules: [ 'price', 'earnings' ] // see the docs for the full list
+      modules: [ 'price', 'earnings' ] // see the doc for the full list
     }, function (err, result) {
       if(result){
         console.log(result.earnings.financialsChart.yearly)
         const earningsGrowth = result.earnings.financialsChart.yearly
         .map((v, i) => {
           if(i === 0) return { annee: v.date, chiffre_affaires: v.revenue, benefice_net: v.earnings, pctChange: 0 }
-          return { annee: v.date, chiffre_affaires: v.revenue, benefice_net: v.earnings, pctChange: calculatePctChange(result.earnings.financialsChart.yearly[i -1].earnings, v.earnings)}
+          return { 
+            annee: v.date, 
+            chiffre_affaires: v.revenue, 
+            chiffre_affaires_pctChange: calculatePctChange(result.earnings.financialsChart.yearly[i -1].revenue, v.revenue),
+            benefice_net: v.earnings, 
+            benefice_net_pctChange: calculatePctChange(result.earnings.financialsChart.yearly[i -1].earnings, v.earnings)
+          }
         });
         setEarningsGain(JSON.stringify(earningsGrowth, null, 2)); 
       }
@@ -33,7 +39,11 @@ const App = () => {
   const getQuotesList = (e) => setTicker(e.target.value);
 
   useEffect(() => {
-    getProfits();
+    if (isInitialMount.current) {
+      isInitialMount.current = false;
+   } else {
+      getProfits();
+   }
   }, [ticker]);
 
   return (
